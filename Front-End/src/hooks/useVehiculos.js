1c4e96e73d4f7d5d7f64b21484c90339a2cd3377
Jose Rojas <joseyx041199@gmail.com>
2023-10-21 19:20:51 -0400

    Added: delete function to each hook

--- Front-End/src/hooks/useVehiculos.js
+++ Front-End/src/hooks/useVehiculos.js
@@ -1,16 +1,32 @@
-import { useState, useEffect } from 'react'
-import { getAllVehiculos } from '../service/Vehiculos.js'
+import { useState, useEffect } from "react";
+import {
+	getAllVehiculos,
+	deleteVehiculo as deleteVehiculoApi,
+} from "../service/Vehiculos.js";
 
 const useVehiculos = () => {
-  const [vehiculos, setVehiculos] = useState([])
+	const [vehiculos, setVehiculos] = useState([]);
 
-  const refreshVehiculos = () => {
-    getAllVehiculos().then(vehiculos => setVehiculos(vehiculos))
-  }
-  useEffect(() => {
-    refreshVehiculos()
-  }, [])
-  return { vehiculos }
-}
+	const refreshVehiculos = () => {
+		getAllVehiculos().then((vehiculos) => setVehiculos(vehiculos));
+	};
 
-export { useVehiculos }
+	useEffect(() => {
+		refreshVehiculos();
+	}, []);
+
+	const deleteVehiculo = async (placa) => {
+		try {
+			await deleteVehiculoApi(placa);
+			setVehiculos((prevVehiculos) =>
+				prevVehiculos.filter((vehiculo) => vehiculo.placa !== placa)
+			);
+		} catch (error) {
+			console.log("Error deleting vehiculo:", error);
+		}
+	};
+
+	return { vehiculos, deleteVehiculo };
+};
+
+export { useVehiculos };
--- Front-End/src/hooks/useViajes.js
+++ Front-End/src/hooks/useViajes.js
@@ -1,16 +1,30 @@
-import { useState, useEffect } from 'react'
-import { getAllViajes } from '../service/Viajes.js'
+import { useState, useEffect } from "react";
+import {
+	getAllViajes,
+	deleteViaje as deleteViajeApi,
+} from "../service/Viajes.js";
 
 const useViajes = () => {
-  const [viajes, setViajes] = useState([])
+	const [viajes, setViajes] = useState([]);
 
-  const refreshViajes = () => {
-    getAllViajes().then(viajes => setViajes(viajes))
-  }
-  useEffect(() => {
-    refreshViajes()
-  }, [])
-  return { viajes }
-}
+	const refreshViajes = () => {
+		getAllViajes().then((viajes) => setViajes(viajes));
+	};
+	useEffect(() => {
+		refreshViajes();
+	}, []);
 
-export { useViajes }
+	const deleteViaje = async (viajeId) => {
+		try {
+			await deleteViajeApi(viajeId);
+			setViajes((prevViajes) =>
+				prevViajes.filter((viaje) => viaje.id !== viajeId)
+			);
+		} catch (error) {
+			console.log("Error deleting viaje:", error);
+		}
+	};
+	return { viajes, deleteViaje };
+};
+
+export { useViajes };
