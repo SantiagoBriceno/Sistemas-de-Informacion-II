16778c60021421d5a549add7901a34feb84b87a7
Santiago <Santiagogbf2000@Gmail.com>
2023-10-23 16:35:23 -0400

    Fixed: Format by Standard

--- Back-End/src/routes/viaje.routes.js
+++ Back-End/src/routes/viaje.routes.js
@@ -1,25 +1,25 @@
-import { Router } from "express";
+import { Router } from 'express'
 import {
-	getViajes,
-	getViaje,
-	createViaje,
-	editViaje,
-	deleteViaje,
-} from "../controller/viaje.controllers.js";
-const router = Router();
+  getViajes,
+  getViaje,
+  createViaje,
+  editViaje,
+  deleteViaje
+} from '../controller/viaje.controllers.js'
+const router = Router()
 
 // RUTA PARA OBETENER TODOS LOS GASTOS DE LA BASE DE DATOS
-router.get("/", getViajes);
+router.get('/', getViajes)
 
 // RUTA PARA OBTENER UN GASTO DE LA BASE DE DATOS
-router.get("/:id", getViaje);
+router.get('/:id', getViaje)
 
 // RUTA PARA CREAR UN NUEVO GASTO EN LA BASE DE DATOS
-router.post("/", createViaje);
+router.post('/', createViaje)
 
 // RUTA PARA EDITAR UN GASTO EN LA BASE DE DATOS
-router.patch("/:id", editViaje);
+router.patch('/:id', editViaje)
 
-router.delete("/:id", deleteViaje);
+router.delete('/:id', deleteViaje)
 
-export default router;
+export default router
--- Back-End/src/services/viaje.service.js
+++ Back-End/src/services/viaje.service.js
@@ -1,37 +1,37 @@
-import { pool } from "../db.js";
+import { pool } from '../db.js'
 
 const getViajes = async () => {
-	const [data] = await pool.query("SELECT * FROM viajes");
-	return data;
-};
+  const [data] = await pool.query('SELECT * FROM viajes')
+  return data
+}
 
 const getViaje = async (id) => {
-	const [data] = await pool.query("SELECT * FROM viajes WHERE id = ?", [id]);
-	return data;
-};
+  const [data] = await pool.query('SELECT * FROM viajes WHERE id = ?', [id])
+  return data
+}
 
 const createViaje = async (viaje) => {
-	const [data] = await pool.query("INSERT INTO viajes SET ?", [viaje]);
-	return data;
-};
+  const [data] = await pool.query('INSERT INTO viajes SET ?', [viaje])
+  return data
+}
 
 const editViaje = async (id, viaje) => {
-	const [data] = await pool.query("UPDATE viajes SET ? WHERE id = ?", [
-		viaje,
-		id,
-	]);
-	return data;
-};
+  const [data] = await pool.query('UPDATE viajes SET ? WHERE id = ?', [
+    viaje,
+    id
+  ])
+  return data
+}
 
 const deleteViaje = async (id) => {
-	const [data] = await pool.query("DELETE FROM viajes WHERE id = ?", [id]);
-	return data;
-};
+  const [data] = await pool.query('DELETE FROM viajes WHERE id = ?', [id])
+  return data
+}
 
 export default {
-	getViajes,
-	getViaje,
-	createViaje,
-	editViaje,
-	deleteViaje,
-};
+  getViajes,
+  getViaje,
+  createViaje,
+  editViaje,
+  deleteViaje
+}
