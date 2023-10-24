080292d681833d3dfe8c32a042490256d374cb3a
Santiago <Santiagogbf2000@Gmail.com>
2023-10-24 10:01:17 -0400

    Fixed: Styles in main view

--- Front-End/src/components/card/vehiculosM.jsx
+++ Front-End/src/components/card/vehiculosM.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import viajesRealizados from './assets/viajesRealizados.webp'
 import conductoresVehiculo from './assets/Conductores.jpg'
+import './assets/mainCard.css'
 
 function MainCard () {
   const [vehiculos, setVehiculos] = useState([])
@@ -39,132 +40,135 @@ function MainCard () {
   }
 
   return (
-    <div className='container'>
-      <div className='rounded overflow-hidden shadow-lg p-4 bg-gray-100'>
+    <>
+      <h1 className='mt-32 ml-80 text-4xl align-text-center'>Multiservicios JoCeSanti C.A</h1>
+      <div className='containerMain' style={{ marginTop: '20px' }}>
+        <div className='sub-container rounded overflow-hidden shadow-lg p-4 bg-gray-100'>
 
-        <div className='table-container'>
-          <img class='w-full pb-5' src={viajesRealizados} alt='Sunset in the mountains' style={{ borderRadius: 0 }} />
-          <h1 className='text-2xl font-bold mb-4 text-center'>Ultimos viajes</h1>
-          <table className='table-auto w-full'>
-            <thead>
-              <tr>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Ubicacion
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Distancia
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Fecha de inicio
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Marca
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Modelo
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Nombre
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {viajes.map((viaje) => (
-                <tr
-                  key={viaje.nombre}
-                  className='hover:bg-gray-200'
-                >
-                  <td className='border px-4 py-2 text-center'>
-                    {viaje.ubicacion}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {viaje.distancia}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {viaje.fechaInicio}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {viaje.marca}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {viaje.modelo}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {viaje.nombre}
-                  </td>
+          <div className='table-container'>
+            <img class='w-full pb-5' src={viajesRealizados} alt='Sunset in the mountains' style={{ borderRadius: 0 }} />
+            <h2 className='text-2xl font-bold mb-4 text-center'>Ultimos viajes</h2>
+            <table className='table-auto w-full'>
+              <thead>
+                <tr>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Ubicacion
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Distancia
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Fecha de inicio
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Marca
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Modelo
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Nombre
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {viajes.map((viaje) => (
+                  <tr
+                    key={viaje.nombre}
+                    className='hover:bg-gray-200'
+                  >
+                    <td className='border px-4 py-2 text-center'>
+                      {viaje.ubicacion}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {viaje.distancia}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {viaje.fechaInicio}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {viaje.marca}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {viaje.modelo}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {viaje.nombre}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
-      </div>
-      <div className='rounded overflow-hidden shadow-lg p-4 bg-gray-100'>
-        <div className='table-container'>
-          <img class='w-full pb-5' src={conductoresVehiculo} alt='Sunset in the mountains' style={{ borderRadius: 0 }} />
-          <h1 className='text-2xl font-bold mb-4 text-center'>Vehiculos con conductor asignado</h1>
-          <table className='table-auto w-full'>
-            <thead>
-              <tr>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Placa del vehiculo
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Marca
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Modelo
-                </th>
-                <th
-                  className='px-4 py-2 text-center cursor-pointer'
-                >
-                  Conductor asignado
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {vehiculos.map((vehiculo) => (
-                <tr
-                  key={vehiculo.placa}
-                  className='hover:bg-gray-200'
-                >
-                  <td className='border px-4 py-2 text-center'>
-                    {vehiculo.placa}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {vehiculo.marca}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {vehiculo.modelo}
-                  </td>
-                  <td className='border px-4 py-2 text-center'>
-                    {vehiculo.nombre}
-                  </td>
+        </div>
+        <div className='sub-container rounded overflow-hidden shadow-lg p-4 bg-gray-100'>
+          <div className='table-container'>
+            <img class='w-full pb-5' src={conductoresVehiculo} alt='Sunset in the mountains' style={{ borderRadius: 0 }} />
+            <h2 className='text-2xl font-bold mb-4 text-center'>Vehiculos con conductor asignado</h2>
+            <table className='table-auto w-full'>
+              <thead>
+                <tr>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Placa del vehiculo
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Marca
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Modelo
+                  </th>
+                  <th
+                    className='px-4 py-2 text-center cursor-pointer'
+                  >
+                    Conductor asignado
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {vehiculos.map((vehiculo) => (
+                  <tr
+                    key={vehiculo.placa}
+                    className='hover:bg-gray-200'
+                  >
+                    <td className='border px-4 py-2 text-center'>
+                      {vehiculo.placa}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {vehiculo.marca}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {vehiculo.modelo}
+                    </td>
+                    <td className='border px-4 py-2 text-center'>
+                      {vehiculo.nombre}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
