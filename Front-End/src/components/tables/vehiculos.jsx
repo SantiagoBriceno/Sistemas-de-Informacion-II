6e7893a36ea37767142dfae6b4a86c6efbdcf018
Jose Rojas <joseyx041199@gmail.com>
2023-10-24 20:45:03 -0400

    Fixed button's classes

--- Front-End/src/components/tables/vehiculos.jsx
+++ Front-End/src/components/tables/vehiculos.jsx
@@ -17,7 +17,7 @@ function Vehiculos () {
   }
 
   const handleEdit = (placa) => {
-    
+
   }
 
   return (
@@ -114,7 +114,7 @@ function Vehiculos () {
                 >
                   Eliminar
                 </button>
-                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4'
                   onClick={() => handleEdit(vehiculo.placa)}
                 >
                   Editar
